f8dbe00f8180292384b704562d4c31ab4e355b60
Adnan.Wani <Adnan.Wani@SXR-QAC-024>
2018-01-08 19:52:44 +0530

    connection string working

--- Fairdeal Kashmir Salary Software/Emp.cs
+++ Fairdeal Kashmir Salary Software/Emp.cs
@@ -26,11 +26,22 @@ namespace Fairdeal_Kashmir_Salary_Software
        
         private void btnSave_Click(object sender, EventArgs e)
         {
+            //` SqlCommand cmd = new SqlCommand("Insert_User", cn);
+            //cmd.CommandType = CommandType.StoredProcedure;
+            //if (cn.State != ConnectionState.Open)
+            //    cn.Open();
+            //cmd.Parameters.Add("Id", SqlDbType.NVarChar).Value = "00A640BD-1A0D-499D-9155-BA2B626D7B68";
+            //cmd.Parameters.Add("AccountId", SqlDbType.NVarChar).Value = "DCBA241B-2B06-48D7-9AC1-6E277FBB1C2A";
+            //cmd.Parameters.Add("FirstName", SqlDbType.NVarChar).Value = "Mark";
+            //cmd.Parameters.Add("LastName", SqlDbType.NVarChar).Value = "Wahlberg";
+            //cmd.Parameters.Add("JobTitle", SqlDbType.NVarChar).Value = "Actor";
+            //cmd.Parameters.Add("PhoneNumber", SqlDbType.NVarChar).Value = "9889898989";
 
+            return cmd.ExecuteNonQuery();
             SqlCommand cmd = new SqlCommand();
            cmd.CommandText= "select * from employee";
-            DataManager.executeNonQuery(cmd);
-
+            DataManager.executeDataset(cmd);
+            txtFNmame.Text = "done";
 
         }
     }
