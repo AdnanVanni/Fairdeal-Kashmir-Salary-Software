44dd8a5d15267acb2631ba62bb99698327550a85
AdnanVanni <adnanvanni@gmail.com>
2018-01-11 23:18:18 -0800

    monthly fe

--- Fairdeal Kashmir Salary Software/Monthly Transaction.Designer.cs
+++ Fairdeal Kashmir Salary Software/Monthly Transaction.Designer.cs
@@ -28,144 +28,295 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtDName = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblFine = new System.Windows.Forms.Label();
+            this.lblTdc = new System.Windows.Forms.Label();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.lblEmployeeNamee = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Ename = new System.Windows.Forms.ComboBox();
+            this.comboBoxYear = new System.Windows.Forms.ComboBox();
+            this.comboBoxMonth = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbLMPFLS = new System.Windows.Forms.Label();
+            this.txtMPFLS = new System.Windows.Forms.TextBox();
+            this.txtNetSalary = new System.Windows.Forms.TextBox();
+            this.txtSaveRecord = new System.Windows.Forms.Button();
+            this.lblMemo = new System.Windows.Forms.Label();
+            this.richTextBoxMemo = new System.Windows.Forms.RichTextBox();
+            this.txtPF = new System.Windows.Forms.TextBox();
+            this.txtAAMD = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPFCalc = new System.Windows.Forms.Label();
+            this.txtMonthlySalary = new System.Windows.Forms.TextBox();
+            this.lblActualSalary = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // lblFine
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(408, 99);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Department";
+            this.lblFine.AutoSize = true;
+            this.lblFine.Location = new System.Drawing.Point(126, 310);
+            this.lblFine.Name = "lblFine";
+            this.lblFine.Size = new System.Drawing.Size(27, 13);
+            this.lblFine.TabIndex = 5;
+            this.lblFine.Text = "Fine";
             // 
-            // label2
+            // lblTdc
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(408, 66);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Department";
+            this.lblTdc.AutoSize = true;
+            this.lblTdc.Location = new System.Drawing.Point(126, 347);
+            this.lblTdc.Name = "lblTdc";
+            this.lblTdc.Size = new System.Drawing.Size(29, 13);
+            this.lblTdc.TabIndex = 6;
+            this.lblTdc.Text = "TDC";
             // 
-            // label3
+            // lblMonth
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(126, 175);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Department";
+            this.lblMonth.AutoSize = true;
+            this.lblMonth.Location = new System.Drawing.Point(126, 111);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(37, 13);
+            this.lblMonth.TabIndex = 7;
+            this.lblMonth.Text = "Month";
             // 
-            // label4
+            // lblEmployeeNamee
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(126, 136);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Department";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(126, 103);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(62, 13);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "Department";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(126, 66);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(62, 13);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Department";
-            // 
-            // txtDName
-            // 
-            this.txtDName.Location = new System.Drawing.Point(487, 195);
-            this.txtDName.Name = "txtDName";
-            this.txtDName.Size = new System.Drawing.Size(118, 20);
-            this.txtDName.TabIndex = 12;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(487, 96);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(118, 20);
-            this.textBox1.TabIndex = 13;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(487, 63);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(118, 20);
-            this.textBox2.TabIndex = 14;
+            this.lblEmployeeNamee.AutoSize = true;
+            this.lblEmployeeNamee.Location = new System.Drawing.Point(126, 74);
+            this.lblEmployeeNamee.Name = "lblEmployeeNamee";
+            this.lblEmployeeNamee.Size = new System.Drawing.Size(53, 13);
+            this.lblEmployeeNamee.TabIndex = 8;
+            this.lblEmployeeNamee.Text = "Employee";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(217, 168);
+            this.textBox3.Location = new System.Drawing.Point(217, 307);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(118, 20);
+            this.textBox3.Size = new System.Drawing.Size(121, 20);
             this.textBox3.TabIndex = 15;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(217, 129);
+            this.textBox4.Location = new System.Drawing.Point(217, 340);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(118, 20);
+            this.textBox4.Size = new System.Drawing.Size(121, 20);
             this.textBox4.TabIndex = 16;
             // 
-            // textBox5
+            // Ename
             // 
-            this.textBox5.Location = new System.Drawing.Point(217, 96);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(118, 20);
-            this.textBox5.TabIndex = 17;
+            this.Ename.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Ename.FormattingEnabled = true;
+            this.Ename.Location = new System.Drawing.Point(217, 66);
+            this.Ename.Name = "Ename";
+            this.Ename.Size = new System.Drawing.Size(121, 21);
+            this.Ename.TabIndex = 18;
             // 
-            // comboBox1
+            // comboBoxYear
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(217, 57);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 18;
+            this.comboBoxYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxYear.FormattingEnabled = true;
+            this.comboBoxYear.Items.AddRange(new object[] {
+            "2001",
+            "2002",
+            "2003",
+            "2004",
+            "2005",
+            "2006",
+            "2007",
+            "2008",
+            "2009",
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2017",
+            "2018",
+            "2019",
+            "2020",
+            "2021",
+            "2022",
+            "2023",
+            "2024",
+            "2025",
+            "2026",
+            "2027",
+            "2028",
+            "2029",
+            "2030",
+            "2031",
+            "2033",
+            "2034",
+            "2035",
+            "2036",
+            "2037",
+            "2038",
+            "2039",
+            "2040",
+            "",
+            ""});
+            this.comboBoxYear.Location = new System.Drawing.Point(307, 103);
+            this.comboBoxYear.Name = "comboBoxYear";
+            this.comboBoxYear.Size = new System.Drawing.Size(84, 21);
+            this.comboBoxYear.TabIndex = 19;
+            // 
+            // comboBoxMonth
+            // 
+            this.comboBoxMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMonth.FormattingEnabled = true;
+            this.comboBoxMonth.Items.AddRange(new object[] {
+            "JAN",
+            "FEB",
+            "MAR",
+            "APR",
+            "MAY",
+            "JUN",
+            "JULY",
+            "AUG",
+            "SEP",
+            "OCT",
+            "NOV",
+            "DEC"});
+            this.comboBoxMonth.Location = new System.Drawing.Point(217, 103);
+            this.comboBoxMonth.Name = "comboBoxMonth";
+            this.comboBoxMonth.Size = new System.Drawing.Size(84, 21);
+            this.comboBoxMonth.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Calculate Salary";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lbLMPFLS
+            // 
+            this.lbLMPFLS.AutoSize = true;
+            this.lbLMPFLS.Location = new System.Drawing.Point(126, 206);
+            this.lbLMPFLS.Name = "lbLMPFLS";
+            this.lbLMPFLS.Size = new System.Drawing.Size(132, 13);
+            this.lbLMPFLS.TabIndex = 22;
+            this.lbLMPFLS.Text = "PF Loan Month Deduction";
+            // 
+            // txtMPFLS
+            // 
+            this.txtMPFLS.Location = new System.Drawing.Point(217, 222);
+            this.txtMPFLS.Name = "txtMPFLS";
+            this.txtMPFLS.Size = new System.Drawing.Size(121, 20);
+            this.txtMPFLS.TabIndex = 23;
+            // 
+            // txtNetSalary
+            // 
+            this.txtNetSalary.Location = new System.Drawing.Point(307, 435);
+            this.txtNetSalary.Name = "txtNetSalary";
+            this.txtNetSalary.Size = new System.Drawing.Size(130, 20);
+            this.txtNetSalary.TabIndex = 24;
+            // 
+            // txtSaveRecord
+            // 
+            this.txtSaveRecord.Location = new System.Drawing.Point(478, 481);
+            this.txtSaveRecord.Name = "txtSaveRecord";
+            this.txtSaveRecord.Size = new System.Drawing.Size(130, 23);
+            this.txtSaveRecord.TabIndex = 25;
+            this.txtSaveRecord.Text = "Save Record";
+            this.txtSaveRecord.UseVisualStyleBackColor = true;
+            // 
+            // lblMemo
+            // 
+            this.lblMemo.AutoSize = true;
+            this.lblMemo.Location = new System.Drawing.Point(475, 74);
+            this.lblMemo.Name = "lblMemo";
+            this.lblMemo.Size = new System.Drawing.Size(64, 13);
+            this.lblMemo.TabIndex = 27;
+            this.lblMemo.Text = "Enter Memo";
+            // 
+            // richTextBoxMemo
+            // 
+            this.richTextBoxMemo.Location = new System.Drawing.Point(478, 90);
+            this.richTextBoxMemo.Name = "richTextBoxMemo";
+            this.richTextBoxMemo.Size = new System.Drawing.Size(217, 96);
+            this.richTextBoxMemo.TabIndex = 28;
+            this.richTextBoxMemo.Text = "";
+            // 
+            // txtPF
+            // 
+            this.txtPF.Location = new System.Drawing.Point(217, 173);
+            this.txtPF.Name = "txtPF";
+            this.txtPF.Size = new System.Drawing.Size(121, 20);
+            this.txtPF.TabIndex = 29;
+            // 
+            // txtAAMD
+            // 
+            this.txtAAMD.Location = new System.Drawing.Point(217, 270);
+            this.txtAAMD.Name = "txtAAMD";
+            this.txtAAMD.Size = new System.Drawing.Size(121, 20);
+            this.txtAAMD.TabIndex = 31;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Advance Loan Month Deduction";
+            // 
+            // lblPFCalc
+            // 
+            this.lblPFCalc.AutoSize = true;
+            this.lblPFCalc.Location = new System.Drawing.Point(116, 180);
+            this.lblPFCalc.Name = "lblPFCalc";
+            this.lblPFCalc.Size = new System.Drawing.Size(79, 13);
+            this.lblPFCalc.TabIndex = 34;
+            this.lblPFCalc.Text = "Provident Fund";
+            // 
+            // txtMonthlySalary
+            // 
+            this.txtMonthlySalary.Location = new System.Drawing.Point(217, 138);
+            this.txtMonthlySalary.Name = "txtMonthlySalary";
+            this.txtMonthlySalary.Size = new System.Drawing.Size(121, 20);
+            this.txtMonthlySalary.TabIndex = 30;
+            // 
+            // lblActualSalary
+            // 
+            this.lblActualSalary.AutoSize = true;
+            this.lblActualSalary.Location = new System.Drawing.Point(126, 141);
+            this.lblActualSalary.Name = "lblActualSalary";
+            this.lblActualSalary.Size = new System.Drawing.Size(69, 13);
+            this.lblActualSalary.TabIndex = 35;
+            this.lblActualSalary.Text = "Actual Salary";
             // 
             // Monthly_Transaction
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 456);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox5);
+            this.ClientSize = new System.Drawing.Size(727, 579);
+            this.Controls.Add(this.lblActualSalary);
+            this.Controls.Add(this.lblPFCalc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAAMD);
+            this.Controls.Add(this.txtMonthlySalary);
+            this.Controls.Add(this.txtPF);
+            this.Controls.Add(this.richTextBoxMemo);
+            this.Controls.Add(this.lblMemo);
+            this.Controls.Add(this.txtSaveRecord);
+            this.Controls.Add(this.txtNetSalary);
+            this.Controls.Add(this.txtMPFLS);
+            this.Controls.Add(this.lbLMPFLS);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxMonth);
+            this.Controls.Add(this.comboBoxYear);
+            this.Controls.Add(this.Ename);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.txtDName);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblEmployeeNamee);
+            this.Controls.Add(this.lblMonth);
+            this.Controls.Add(this.lblTdc);
+            this.Controls.Add(this.lblFine);
             this.Name = "Monthly_Transaction";
             this.Text = "Monthly_Transaction";
             this.Load += new System.EventHandler(this.Monthly_Transaction_Load);
@@ -175,19 +326,27 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtDName;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblFine;
+        private System.Windows.Forms.Label lblTdc;
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.Label lblEmployeeNamee;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox Ename;
+        private System.Windows.Forms.ComboBox comboBoxYear;
+        private System.Windows.Forms.ComboBox comboBoxMonth;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbLMPFLS;
+        private System.Windows.Forms.TextBox txtMPFLS;
+        private System.Windows.Forms.TextBox txtNetSalary;
+        private System.Windows.Forms.Button txtSaveRecord;
+        private System.Windows.Forms.Label lblMemo;
+        private System.Windows.Forms.RichTextBox richTextBoxMemo;
+        private System.Windows.Forms.TextBox txtPF;
+        private System.Windows.Forms.TextBox txtAAMD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPFCalc;
+        private System.Windows.Forms.TextBox txtMonthlySalary;
+        private System.Windows.Forms.Label lblActualSalary;
     }
 }
